3c9da49ad282b58776d65b3ff68ef03fdcf0643d
Artur Tamborski <tamborskiartur@gmail.com>
2019-04-30 20:04:24 +0200

    convert to class based views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from rest_framework.parsers import JSONParser
-from annoying.functions import get_object_or_None
+from rest_framework import parsers
+from rest_framework import generics
 
 from backend.api import models
 from backend.api import serializers
@@ -12,69 +12,55 @@ from backend.api import serializers
 @csrf_exempt
 @require_http_methods(['GET'])
 def top(request):
-    model = models.Movie.objects.all()
-    serializer = serializers.Movie(model, many=True)
-    print('check what is serializer and how to add new items')
-    breakpoint()
+    objs = models.Movie.objects.all()
+    serializer = serializers.Movie(objs, many=True)
+
+    for idx, data in enumerate(serializer.data):
+        data['position'] = idx + 1
+
     return JsonResponse(serializer.data, safe=False)
 
 
-@csrf_exempt
-@require_http_methods(['GET', 'POST'])
-def movies(request):
-    if request.method == 'GET':
+class Movie(generics.ListCreateAPIView):
+    serializer_class = serializers.Movie
+    queryset = serializer_class.Meta.model.objects.all()
+    parser_classes = (parsers.JSONParser,)
+
+    def get(self, request, *args, **kwargs):
         sort = request.GET.get('sort')
-        only = request.GET.get('only')
 
-        if only and only.split(',') in serializers.Movie.Meta.fields:
-            objs = models.Movie.objects.values_list(only)
-        else:
-            objs = models.Movie.objects.all()
+        objects = self.serializer_class.Meta.model.objects.all()
 
-        if sort and sort in serializers.Movie.Meta.fields:
-            objs = objs.order_by(sort)
+        # todo: move sorting and filtering to serializer
+        if sort:
+            objects = objects.order_by(sort)
 
-        serializer = serializers.Movie(objs, many=True)
+        serializer = self.serializer_class(objects, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    data = JSONParser().parse(request)
-    serializer = serializers.Movie(data=data)
-
-    if serializer.is_valid(raise_exception=True):
-        title = serializer['title']
-        obj = get_object_or_None(models.Movie, title__icontains=title)
-
-        if obj:
-            return JsonResponse(serializers.Movie(data=obj, status=201))
-
-        # not present in db, need to fetch it
-        print('fetching from external db...')
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=201)
+        return JsonResponse(data=serializer.errors, status=400)
 
 
+class Comment(generics.ListCreateAPIView):
+    serializer_class = serializers.Comment
+    queryset = serializer_class.Meta.model.objects.all()
+    parser_classes = (parsers.JSONParser,)
 
-@csrf_exempt
-@require_http_methods(['GET', 'POST'])
-def comments(request):
-    if request.method == 'GET':
-        sort = request.GET.get('sort')
-        only = request.GET.get('only')
-
-        if only and only.split(',') in serializers.Comment.Meta.fields:
-            objs = models.Comment.objects.values_list(only)
-        else:
-            objs = models.Comment.objects.all()
-
-        if sort and sort in serializers.Comment.Meta.fields:
-            objs = objs.order_by(sort)
-
-        serializer = serializers.Comment(objs, many=True)
+    def get(self, request, *args, **kwargs):
+        objects = self.serializer_class.Meta.model.objects.all()
+        serializer = self.serializer_class(objects, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    data = JSONParser().parse(request)
-    serializer = serializers.Comment(data=data)
-
-    if serializer.is_valid(raise_exception=True):
-        movie_id = serializer['id']
-        text = serializer['text']
-        serializer.save()
-        return JsonResponse(serializer.data, status=201)
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=201)
+        return JsonResponse(data=serializer.errors, status=400)
