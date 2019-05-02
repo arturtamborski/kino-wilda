a939a37b77b670fc6e3707209aa1b4acc7f3b032
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-02 16:06:45 +0200

    add api key config

--- backend/settings/config.py
+++ backend/settings/config.py
@@ -1,8 +1,11 @@
 class Config:
 
+    # public
     DEBUG = True
-
     SECRET_KEY = ''
-
+    API_KEY = ''
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+    # private
+    omdb_url = 'http://www.omdbapi.com/'
 
