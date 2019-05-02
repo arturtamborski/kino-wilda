7d41bb604d3c473335bb73196ec559bed18f6e51
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-02 17:01:11 +0200

    rename field in comment

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,9 +4,9 @@ from backend.api import querysets
 
 
 class Movie(models.Model):
-    title   = models.CharField(max_length=200, unique=True)
-    year    = models.PositiveSmallIntegerField()
-    plot    = models.TextField()
+    title = models.CharField(max_length=200, unique=True)
+    year = models.PositiveSmallIntegerField()
+    plot = models.TextField()
 
     objects = querysets.Movie.as_manager()
 
@@ -18,14 +18,14 @@ class Movie(models.Model):
 
 
 class Comment(models.Model):
-    text    = models.CharField(max_length=200)
-    date    = models.DateTimeField(auto_now_add=True, blank=True)
-    movie   = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
     objects = querysets.Comment.as_manager()
 
     class Meta:
-        ordering = ('movie',)
+        ordering = ('movie_id',)
 
     def __str__(self):
         return self.text[:10]
