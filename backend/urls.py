09bb9cb4c04e2442bf932f228eef8a8045530653
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-02 18:26:12 +0200

    name the urls

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from backend.api import views
 
 urlpatterns = [
-    path('top/', views.top),
-    path('movies/', views.Movie.as_view()),
-    path('comments/', views.Comment.as_view()),
+    path('top/', views.top, name='top'),
+    path('movies/', views.Movie.as_view(), name='movies'),
+    path('comments/', views.Comment.as_view(), name='comments'),
 ]
