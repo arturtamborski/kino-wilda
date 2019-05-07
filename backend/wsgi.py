ab1b25ef72a69c812252143d83c08b8da6a9ca93
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-07 18:12:52 +0200

    actually read the docs first...

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,10 +2,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
