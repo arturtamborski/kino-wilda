77a3782d54d3762c869e7ae287e377e0e4feda9f
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-07 20:23:06 +0200

    create new variable PRODUCTION

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -116,9 +116,8 @@ LOGGING = {
 }
 
 
-if not Config.DEBUG:
+if Config.PRODUCTION:
     MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     DATABASES['default'] = dj_database_url.config(
         conn_max_age=600, ssl_require=True)
--- backend/settings/config.py
+++ backend/settings/config.py
@@ -2,10 +2,13 @@ class Config:
 
     # public
     DEBUG = False
+
+    PRODUCTION = False
+
     SECRET_KEY = ''
     API_KEY = ''
+
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
     # private
     omdb_url = 'http://www.omdbapi.com/'
-
